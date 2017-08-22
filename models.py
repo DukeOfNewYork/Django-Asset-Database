ac3d79adb39679330969ca41b8df2b4bd71f0e77
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2017-08-22 17:12:30 -0400

    Adds Week Of model
    Adds the Week Of model so that assets can be tracked on a week by week basis

--- models.py
+++ models.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.forms import ModelForm
 from django import forms
 
-# Create your models here.
-
-
 class Building(models.Model):
-    name = models.CharField(max_length=100)    
+    name = models.CharField(max_length=100)
     def __str__(self):
         return (self.name)
- 
+
+class WeekOf(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return (self.name)
+    
 class Asset(models.Model):
     name = models.CharField(max_length=100)
     location = models.ForeignKey(Building, related_name='Asset')
     room = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('%Y-%m-%d %H:%M:%S', auto_now=True)
+    pub_date = models.DateTimeField('%Y-%m-%d %H:%M', auto_now=True)
+    week_of = models.ForeignKey(WeekOf, related_name='Asset')
     image = models.ImageField(upload_to='asset_images',default='default.png',blank=True)
     def __str__(self):
         return (self.name + " " + self.location.name )
