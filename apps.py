4ea43239baa2b9fcd8fbdc5715e7c705d3486914
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2017-08-22 17:10:44 -0400

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
 
-
-class TempsiteConfig(AppConfig):
+class MainsiteConfig(AppConfig):
     name = 'DjangoAssetManagement'
