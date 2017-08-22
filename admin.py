df16583d91d47f581c70986647fa265ba5c22c1d
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2017-08-22 17:09:27 -0400

    Adds Week Of
    Adds the Week Of model to the admin view

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Asset
-from .models import Building
-# Register your models here.
+from .models import Asset, Building, WeekOf
+
 
 admin.site.register(Asset)
 admin.site.register(Building)
+admin.site.register(WeekOf)
