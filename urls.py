30688d49f715af805abb22742402c8139edb32fc
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2017-08-22 17:13:53 -0400

    Week Of URLS
    Adds the Week Of URLS

--- urls.py
+++ urls.py
@@ -5,15 +5,16 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^add_assets/$', views.add_assets, name='add_assets'),
-    url(r'^add_image/$', views.add_image, name='add_image'),
-    url(r'^all_assets/$', views.all_assets, name='list_locations'),
     url(r'^building/(\w+)/$', views.building_list, name='building_list'),
+    url(r'^all_assets/$', views.all_assets, name='list_locations'),
+    url(r'^all_weeks/$', views.all_weeks, name='all_weeks'),
+    url(r'^all_weeks/([-\w]+)/$', views.week_of_list, name='week_of_list'),
+    url(r'^export_week/([-\w]+)/$', views.export_week, name='export_weeks'),
     url(r'^search-form/$', views.search_form),
-    url(r'^search/$', views.search),
     url(r'^export/$', views.export_moves, name='export'),
+    url(r'^add_assets/$', views.add_assets, name='add_assets'),
 ]
-if settings.DEBUG:
-    urlpatterns += [url(r'^media/(?P<path>.*)$', serve,
+
+urlpatterns += [url(r'^media/(?P<path>.*)$', serve,
                         {'document_root': settings.MEDIA_ROOT,}),
                     ]
