b997ff75837364c718056f204bfe1f847ffc28b7
Simon Gray <simondgrayiscool@gmail.com>
2018-05-10 20:15:37 -0400

    Adjusted to include a prebuilt Django instance

--- AssetDatabase/models.py
+++ AssetDatabase/models.py
@@ -25,12 +25,12 @@ class WeekOf(models.Model):
 
 
 class Asset(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    location = models.ForeignKey(Building, related_name='Asset')
+    location = models.ForeignKey(Building, related_name='Asset', on_delete=models.CASCADE)
     room = models.CharField(max_length=100)
     pub_date = models.DateTimeField('%Y-%m-%d %H:%M', auto_now=True)
-    week_of = models.ForeignKey(WeekOf, related_name='Asset')
+    week_of = models.ForeignKey(WeekOf, related_name='Asset', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='asset_images', default='default.png', blank=True)
     optional_field = models.CharField(max_length=100, blank=True)
 
